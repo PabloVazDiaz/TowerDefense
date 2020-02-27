6f397ff0e74fc62d2a96ad0743efc05ce1749f8c
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-02-27 20:12:03 +0100

    Explore Grid Neighbours

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent(typeof(Waypoint))]
+public class CubeEditor : MonoBehaviour
+{
+
+    
+    int GridSize;
+    int GridLimitSize;
+    
+    
+    [SerializeField] bool ViewGrid = false;
+    Vector2 snapPos;
+
+    Waypoint waypoint;
+
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+        GridSize = waypoint.GetGridSize();
+        GridLimitSize = waypoint.getGridLimitSize();
+    }
+    
+    void Update()
+    {
+        snapPos = waypoint.GetGridPos() * GridSize;
+        SnapToGrid();
+        UpdateLabel(snapPos);
+        
+    }
+
+    private void SnapToGrid()
+    {
+        transform.position = new Vector3(snapPos.x,0f,snapPos.y);
+    }
+
+    private void UpdateLabel(Vector2 snapPos)
+    {
+        TextMesh textLabel = GetComponentInChildren<TextMesh>();
+        string LabelText = $"{snapPos.x / GridSize } , {snapPos.y / GridSize }";
+        textLabel.text = LabelText;
+        gameObject.name = $"Cube {LabelText}";
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (ViewGrid)
+        {
+
+            Gizmos.color = Color.white;
+            for (int i = 0; i <= GridLimitSize; i++)
+            {
+                for (int j = 0; j <= GridLimitSize; j++)
+                {
+                    Gizmos.DrawWireCube(new Vector3(GridLimitSize * i, 0, GridLimitSize * j) , new Vector3(GridSize, 0, GridSize));
+                }
+
+            }
+
+            Gizmos.color = Color.red;
+            Vector3 gridSizeVector = new Vector3(GridLimitSize * 10, 0, GridLimitSize * 10);
+            Gizmos.DrawWireCube((gridSizeVector) / 2, gridSizeVector + new Vector3(10f, 0, 10f));
+        }
+    }
+}
---
+++ Assets/Waypoint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] const int GridSize = 10;
+    [SerializeField] [Range(1f, 20f)] const int GridLimitsSize = 10;
+    Vector2 gridPos;
+
+    public int GetGridSize()
+    {
+        return GridSize;
+    }
+
+    public Vector2Int GetGridPos()
+    {
+        Vector2Int snapPos = new Vector2Int();
+        snapPos.x = Mathf.RoundToInt(transform.position.x / GridSize) ;
+        snapPos.x = Mathf.Clamp(snapPos.x, 0, GridLimitsSize * GridSize);
+        snapPos.y = Mathf.RoundToInt(transform.position.z / GridSize) ;
+        snapPos.y = Mathf.Clamp(snapPos.y, 0, GridLimitsSize * GridSize);
+        return snapPos;
+
+    }
+
+    internal int getGridLimitSize()
+    {
+        return GridLimitsSize;
+    }
+
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
+        topMeshRenderer.material.color = color;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
